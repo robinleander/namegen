aaa5584cbd42b6650ee10c7f56f82a8be1cf4e98
robinleander <rleanderschroeder@gmail.com>
2018-06-08 15:25:25 +0200

    Add checks

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import random
 import argparse
+import requests
 
 class Source:
     def __init__(self, fname):
@@ -14,11 +15,21 @@ class Source:
                 self.list = list(filter(lambda x: len(x) > 0, f.read().split('\n')))
         return random.choice(self.list)
 
+
 def genName(sources):
     name = ''
     for source in sources:
         name += random.choice(source).random_choice().capitalize()
     return name
+
+
+def twitter_check(name):
+    return requests.get('https://twitter.com/' + name).status_code == 404
+
+
+def lol_check(name):
+    return 'is available' in requests.get('https://lolnames.gg/en/euw/' + name).text
+
 
 def main():
     ATTRIBS = 'adjectives.txt'
@@ -32,7 +43,11 @@ def main():
     parser.add_argument("count", help="number of names to generate")
     args = parser.parse_args()
     for _ in range(10 if args.count is None else int(args.count)):
-        print(genName(sources))
+        name = genName(sources)
+        if all(f(name) for f in [twitter_check, lol_check]):
+            print("\033[1;32m{}\033[0m".format(name))
+        else:
+            print("\033[1;31m{}\033[0m".format(name), end='')
 
 
 if __name__ == '__main__':
